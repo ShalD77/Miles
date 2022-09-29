27955a108ed0ff35d24d4a0baab211f13deef386
Shal <kudrja100@mail.ru>
2022-09-29 13:11:41 +0200

    Рассчитал бонусные мили

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 50_000;
+        int miles = price / 20;
+
+        System.out.println("Бонус:" + miles);
+    }
+}
